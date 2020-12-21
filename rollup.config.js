0090aaeda0216b27c66fd2dee63d23e153eb8711
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-12-21 14:47:56 -0800

    build: Remove too-clever extension remapping

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 const { chmodSync } = require('fs');
 const { builtinModules } = require('module');
-const { dirname, resolve } = require('path');
 const ts = require('@wessberg/rollup-plugin-ts');
 const pkg = require('./package.json');
 
@@ -18,22 +17,6 @@ function executableFile() {
   };
 }
 
-/**
- * Make fancy TS ESM loader work, but avoid breaking Rollup.
- */
-function translateExtensions() {
-  return {
-    name: 'translate extensions',
-    resolveId(source, importer) {
-      if (importer && source.startsWith('.') && source.endsWith('.js')) {
-        return resolve(dirname(importer), source.replace(/\.js$/, '.ts'));
-      }
-      // defer to other resolvers
-      return null;
-    },
-  };
-}
-
 module.exports = [
   {
     input: 'src/api.ts',
@@ -41,7 +24,7 @@ module.exports = [
       { format: 'cjs', file: pkg.exports['.'].default, esModule: false },
       { format: 'esm', file: pkg.exports['.'].import, exports: 'named' },
     ],
-    plugins: [translateExtensions(), ts()],
+    plugins: [ts()],
     external,
   },
   {
@@ -54,7 +37,7 @@ module.exports = [
         banner: '#!/usr/bin/env node\n',
       },
     ],
-    plugins: [translateExtensions(), ts(), executableFile()],
+    plugins: [ts(), executableFile()],
     external,
   },
 ];
