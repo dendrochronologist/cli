85f92986c7402535359f108c00bdd12fbf22011d
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2020-12-31 16:26:07 -0800

    chore: Typecheck rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,16 @@ const { builtinModules } = require('module');
 const ts = require('@wessberg/rollup-plugin-ts');
 const pkg = require('./package.json');
 
-const external = Object.freeze([
+/** @type {import('rollup').ExternalOption} */
+const external = [
   ...builtinModules,
   ...Object.keys(pkg.dependencies),
   // explicit subpath import allowlist (this should be very short)
   'yargs/yargs',
   'yargs/helpers',
-]);
+];
 
+/** @type {import('rollup').RollupOptions} */
 module.exports = {
   input: ['src/api.ts', 'src/cli.ts'],
   output: [
@@ -24,6 +26,7 @@ module.exports = {
       entryFileNames: '[name].mjs',
     },
   ],
+  // @ts-expect-error ("default export" broken from CJS)
   plugins: [ts()],
   external,
 };
