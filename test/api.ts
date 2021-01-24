15396075cadbe4f5b8758f18de1c5f5c3e3c9e91
Daniel Stockman <5605+evocateur@users.noreply.github.com>
2021-01-24 14:14:48 -0800

    test(api): Pull log collection into top-level

--- test/api.ts
+++ test/api.ts
@@ -3,13 +3,20 @@ import path from 'path';
 import util from 'util';
 import { run } from '../src/api';
 
+const logged: Map<string, string[]> = new Map();
+
+process.on('log', (level, _prefix, ...args: string[]) => {
+  const msg =
+    args.length > 1 ? util.format(args[0], ...args.slice(1)) : args[0];
+  logged.set(level, (logged.get(level) || []).concat(msg));
+});
+
+t.afterEach((done) => {
+  logged.clear();
+  done();
+});
+
 t.test('run()', (t): void => {
-  const logged: Map<string, string[]> = new Map();
-  process.on('log', (level, _prefix, ...args: string[]) => {
-    const msg =
-      args.length > 1 ? util.format(args[0], ...args.slice(1)) : args[0];
-    logged.set(level, (logged.get(level) || []).concat(msg));
-  });
   run({});
   t.is(logged.get('resume')?.length, 1, 'logger.resume()');
   t.is(logged.get('info')![0], 'Count those tree rings!');
